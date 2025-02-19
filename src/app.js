3eaf646f9db1ea1c7369ee2a4ded817d30eaa82f
Dinesh Kumar <dkkumar97855@gmail.com>
2025-02-19 17:15:13 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,28 +1,35 @@
 const express = require('express')
-
+const connectDB = require('../config/database')
+const User = require('../model/user')
 const app = express();
 port = 3000
 
-const {adminAuth, userAuth} = require('../middlewares/auth')
-app.use("/admin", adminAuth)
+// const { adminAuth, userAuth } = require('../middlewares/auth')
+// app.use("/admin", adminAuth)
 
-app.post('/user', userAuth, (req, res) => {
-    res.send('data added successfully')
-
+app.post('/signup', (req, res) => {
+    const user = new User({
+        firstName: "Shree Ramchandra",
+        lastName: "dasharath putr",
+        emailId: "ram99@gmail.com",
+        age: 0,
+        password: "12345567890",
+    })
+    user.save();
+    res.send("data post successfully")
 })
 
-app.get('/admin/getAllData', (req, res) => {
-    res.send('data getting success')
 
-})
-
-app.get('/admin/delete', (req, res) => {
-    res.send('delete all the data')
-
-})
-
+connectDB().then(() => {
+    console.log("connect to db");
 
     app.listen(3000, () => {
         console.log(`server is running at port ${port}`);
 
     })
+
+}).catch((err) => {
+    console.log("db not connected");
+
+})
+
