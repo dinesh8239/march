dd4dde35e6fd3eb1fd43f13ba421268898b9086a
Dinesh Kumar <dkkumar97855@gmail.com>
2025-02-19 18:50:34 +0530

    dynamic data in signup api

--- src/app.js
+++ src/app.js
@@ -2,21 +2,26 @@ const express = require('express')
 const connectDB = require('../config/database')
 const User = require('../model/user')
 const app = express();
+app.use(express.json())
 port = 3000
 
 // const { adminAuth, userAuth } = require('../middlewares/auth')
 // app.use("/admin", adminAuth)
 
-app.post('/signup', (req, res) => {
-    const user = new User({
-        firstName: "Shree Ramchandra",
-        lastName: "dasharath putr",
-        emailId: "ram99@gmail.com",
-        age: 0,
-        password: "12345567890",
-    })
-    user.save();
-    res.send("data post successfully")
+app.post('/signup', async (req, res) => {
+    // console.log(req.body);
+
+    const user = new User(req.body)
+    try {
+
+        await user.save();
+        res.send("data post successfully")
+
+    } catch (err) {
+
+        res.status(500).send('something went wrong', err)
+    }
+
 })
 
 
